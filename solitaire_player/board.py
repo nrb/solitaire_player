e3f6876a3d12b052b31705b26c334c879df3305d
Nolan Brubaker <nolan@nbrubaker.com>
2017-08-27 19:42:39 -0400

    Add rough sketch of board and pile structures

---
+++ solitaire_player/board.py
@@ -0,0 +1,19 @@
+"""
+The board and associated structures that actually hold deck and piles
+"""
+
+
+class Board:
+    pass
+
+
+class Pile:
+    pass
+
+
+class TableauPile:
+    pass
+
+
+class FoundationPile:
+    pass
